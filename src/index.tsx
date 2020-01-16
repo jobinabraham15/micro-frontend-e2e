c5094eccaa4628379f2d1abdaa2499cb66b14090
Jobin Abraham <jobin.abraham@aasaanjobs.com>
2020-01-16 11:08:05 +0530

    index json

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import * as serviceWorker from "./serviceWorker";
 declare global {
   interface Window {
     renderE2E: (root: any, containerId: string, history?: any) => void;
+    unmountE2E: (root: any, containerId: string) => void;
   }
 }
 
@@ -17,6 +18,10 @@ window.renderE2E = (root, containerId, history) => {
 if (process.env.NODE_ENV === "development") {
   window.renderE2E(window.document, "root");
 }
+
+window.unmountE2E = (root, containerId) => {
+  ReactDOM.unmountComponentAtNode(root.getElementById(containerId));
+};
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
